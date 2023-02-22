c17c6600ac8f956072ff5f1617b1b31b59efaf27
aminebarka1212@gmail.com <aminebarka1212@gmail.com>
2023-02-22 23:04:22 +0100

    feat: add the Views

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,99 @@
+﻿using Gestion_des_employes.Models.Repositories;
+using Gestion_des_employes.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestion_des_employes.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        readonly IRepository<Employee> employeeRepository;
+        // Injection de dépendance
+        public EmployeeController(IRepository<Employee> empRepository)
+        {
+            employeeRepository = empRepository;
+        }
+
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            var employees = employeeRepository.GetAll();
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+            return View(employee);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                employeeRepository.Add(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+            return View(employee);
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee newEmployee)
+        {
+            try
+            {
+                employeeRepository.Update(id, newEmployee);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+            return View(employee);
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                employeeRepository.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+    }
---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestion_des_employes.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Departement { get; set; }
+        public int Salary { get; set; }
+    }
+}
